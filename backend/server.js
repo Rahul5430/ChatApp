9b5883a8f9dde67e76d5b0bc774076793bf7cf05
Rahul5430 <rahul213himani@gmail.com>
2023-01-22 23:12:24 +0530

    added config

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require("path");
 require('dotenv').config();
 
+connectDB();
 const app = express();
 
 app.use(express.json());
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname1, 'frontend', 'build', 'index.html')
+		)
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running..');
+	});
+}
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.render('API is running');
+	res.render('API is running');
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}...`.yellow.bold);
+	console.log(`Server running on PORT ${PORT}...`.yellow.bold);
 });
