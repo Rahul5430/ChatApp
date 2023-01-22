4fd56db92e69cb0f5e4bd9c02c484c295d070158
Rahul5430 <rahul213himani@gmail.com>
2023-01-22 23:22:17 +0530

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const path = require("path");
 require('dotenv').config();
 
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(express.json());  // to accept json data
+
+// --------------------------deployment------------------------------
 
 const __dirname1 = path.resolve();
 
@@ -24,11 +27,13 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-const PORT = process.env.PORT || 3001;
+// --------------------------deployment------------------------------
 
-app.get('/', (req, res) => {
-	res.render('API is running');
-});
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
 	console.log(`Server running on PORT ${PORT}...`.yellow.bold);
